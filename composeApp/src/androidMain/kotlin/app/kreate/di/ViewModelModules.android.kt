9d732167f9cb5e36641d53923b29e65fe6e45b9a
knighthat <git@knighthat.me>
2026-03-22 16:59:41 -0500

    extract next songs in queue to separate function with viewmodel
    (cherry picked from commit 9282b5277e45f32be898ad422c6d372d01ad751e)

--- composeApp/src/androidMain/kotlin/app/kreate/di/ViewModelModules.android.kt
+++ composeApp/src/androidMain/kotlin/app/kreate/di/ViewModelModules.android.kt
@@ -3,7 +3,9 @@ package app.kreate.di
 import app.kreate.android.viewmodel.LocalPlaylistViewModel
 import app.kreate.android.viewmodel.YouTubePlaylistViewModel
 import app.kreate.android.viewmodel.YoutubeArtistViewModel
+import app.kreate.android.viewmodel.player.ActionBarNextSongsViewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -12,4 +14,8 @@ actual val viewModelModule: Module = module {
     viewModelOf( ::YoutubeArtistViewModel )
     viewModelOf( ::YouTubePlaylistViewModel )
     viewModelOf( ::LocalPlaylistViewModel )
+
+    viewModel { params ->
+        ActionBarNextSongsViewModel( params.get() )
+    }
 }
